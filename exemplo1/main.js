b1ab59576c771d9b487329554b05c87aadab305b
DanBaDo <byte4life@pm.me>
2023-02-04 19:20:30 +0100

    Adds documentation and changes function name.

--- exemplo1/main.js
+++ exemplo1/main.js
@@ -1,8 +1,19 @@
-const button = document.querySelector("button")
+const sillyButton = document.querySelector("button")
 const paragraph = document.querySelector("p")
 
-function count() {
+/**
+ * As seguintes liñas crean una función con nome sillyCount.
+ * Cando esta función sexa invocada no futuro executará
+ * as instruccións almacenadas no bloque entre os signos {}.
+ */
+function sillyCount() {
     paragraph.innerText += "Ouch!... "
 }
 
-button.addEventListener("click", count)
+/**
+ * A seguinte liña establece un "onclick" no elemento 
+ * HTML almacenado na variable sillyButton.
+ * Consegue que cando se faga click no botón, se poña en
+ * marcha a funcion "sillyCount".
+ */
+sillyButton.addEventListener("click", sillyCount)
